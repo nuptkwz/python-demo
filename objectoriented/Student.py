72ac9089f1b9764acd8fc1e1336645efb2ef4d32
keweizhou <1514991931@qq.com>
2024-05-20 11:15:39 +0800

    私有变量访问

--- objectoriented/Student.py
+++ objectoriented/Student.py
@@ -1,13 +1,25 @@
-## 面向对象示例
+## 面向对象示例,以Student类为例，在Python中，定义类是通过class关键字
+## 注意到__init__方法的第一个参数永远是self，表示创建的实例本身，因此，在__init__方法内部，就可以把各种属性绑定到self，因为self就指向创建的实例本身。
 class Student(object):
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    def __init__(self, name, score):
+        self.__name = name
+        self.score = score
 
     def print_score(self):
-        print('%s: %s' % (self.name, self.age))
+        print('%s: %s' % (self.__name, self.score))
+
+    def grade(self):
+        if self.score >= 90:
+            return 'A'
+        elif self.score >= 60:
+            return 'B'
+        else:
+            return 'C'
 
 
 bar = Student('Bar', 59)
 bar.print_score()
+print(bar.grade())
+
+## 和普通的函数相比，在类中定义的函数只有一点不同，就是第一个参数永远是实例变量self，并且，调用时，不用传递该参数。除此之外，类的方法和普通函数没有什么区别，所以，你仍然可以用默认参数、可变参数、关键字参数和命名关键字参数。
---
+++ objectoriented/Teacher.py
@@ -0,0 +1,4 @@
+from objectoriented.Student import Student
+
+bar = Student('Bar', 59)
+print('bar.name:', bar.name)
